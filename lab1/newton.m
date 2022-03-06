001e6ce0e9d4b160eaf786e53536d6ca08abe6cd
Michał Szopiński <mszopinski@mssnt.pl>
2022-03-06 19:47:56 +0100

    Add Newton's method

--- lab1/newton.m
+++ lab1/newton.m
@@ -4,5 +4,22 @@
 % This file implements Newton's method.
 
 function xAsterisk = newton(func, startingPoint, stopCond, stopCondArg)
-    xAsterisk = startingPoint(1);
+    stepCount = 0;
+    startTime = clock();
+    
+    xAsterisk = startingPoint;
+    y = func(startingPoint);
+
+    while 1
+        if stopCond == StopCondition.DesiredValue && y <= stopCondArg; break; end
+
+        deriv1 = deriv(func, xAsterisk, 1);
+        deriv2 = deriv(func, xAsterisk, 2);
+        xAsterisk = xAsterisk - deriv1 / deriv2;
+        y = func(xAsterisk);
+
+        stepCount = stepCount + 1;
+        if stopCond == StopCondition.MaxIterations && stepCount == stopCondArg; break; end
+        if stopCond == StopCondition.MaxTime && etime(clock(), startTime) >= stopCondArg; break; end
+    end
 end
