81ec1b9767bca0a42fc8ba450058857a33253d27
Michał Szopiński <mszopinski@mssnt.pl>
2022-03-07 20:25:03 +0100

    Implement gradient descent

--- lab1/gradientDescent.m
+++ lab1/gradientDescent.m
@@ -4,5 +4,24 @@
 % This file implements the gradient descent method.
 
 function xAsterisk = gradientDescent(func, startingPoint, stopCond, stopCondArg)
-    xAsterisk = startingPoint(1);
+    stepCount = 0;
+    startTime = clock();
+
+    nanColumn = NaN * ones(length(startingPoint), 1);
+    prevX = [nanColumn, startingPoint];
+    prevGrad = [nanColumn, gradient(func, startingPoint, 1)];
+
+    while 1
+        if stopCond == StopCondition.DesiredValue && y <= stopCondArg; break; end
+
+        % apply gradient descent formula
+        xAsterisk = prevX(:, 2) - 0.001 * prevGrad(:, 2);
+
+        stepCount = stepCount + 1;
+        prevX = [prevX(:, 2), xAsterisk];
+        prevGrad = [prevGrad(:, 2), gradient(func, xAsterisk, 1)];
+
+        if stopCond == StopCondition.MaxIterations && stepCount == stopCondArg; break; end
+        if stopCond == StopCondition.MaxTime && etime(clock(), startTime) >= stopCondArg; break; end
+    end
 end
