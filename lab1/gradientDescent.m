9069e7a3fc15117516ee00ccde8d741ab076823d
Michał Szopiński <mszopinski@mssnt.pl>
2022-03-07 20:37:42 +0100

    Simplify gradient descent

--- lab1/gradientDescent.m
+++ lab1/gradientDescent.m
@@ -7,19 +7,18 @@ function xAsterisk = gradientDescent(func, startingPoint, stopCond, stopCondArg)
     stepCount = 0;
     startTime = clock();
 
-    nanColumn = NaN * ones(length(startingPoint), 1);
-    prevX = [nanColumn, startingPoint];
-    prevGrad = [nanColumn, gradient(func, startingPoint, 1)];
+    prevX = startingPoint;
+    prevGrad = gradient(func, startingPoint, 1);
 
     while 1
         if stopCond == StopCondition.DesiredValue && y <= stopCondArg; break; end
 
         % apply gradient descent formula
-        xAsterisk = prevX(:, 2) - 0.001 * prevGrad(:, 2);
+        xAsterisk = prevX - 0.001 * prevGrad;
 
         stepCount = stepCount + 1;
-        prevX = [prevX(:, 2), xAsterisk];
-        prevGrad = [prevGrad(:, 2), gradient(func, xAsterisk, 1)];
+        prevX = xAsterisk;
+        prevGrad = gradient(func, xAsterisk, 1);
 
         if stopCond == StopCondition.MaxIterations && stepCount == stopCondArg; break; end
         if stopCond == StopCondition.MaxTime && etime(clock(), startTime) >= stopCondArg; break; end
